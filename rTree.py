c008ada33413cb90eaae4f0c68122c1fd3a79d8f
harrisonhughes <99835918+harrisonhughes@users.noreply.github.com>
2023-12-04 17:11:32 -0600

    Update rTree.py

--- rTree.py
+++ rTree.py
@@ -1,60 +1,93 @@
 # CS 430 Project
 # Harrison Hughes
-# R Tree nearest neighbor algorithm to calculate the skyline of a 2D dataset
+# Nearest neighbor algorithm without R Tree storage to calculate the skyline of a 2D dataset
 
 import numpy as np
-from rtree import index 
+import math as math
+import sys as sys
 
 def rTree(dataSet):
     """
-    Calculate the skyline of a dataset using an R Tree and the nearest neighbor method
+    Calculate the skyline of a dataset using an "R Tree" and the nearest neighbor method
 
     @param dataSet: full dataset of value pairs in shich we will find the skyline
     skyline: holds the full data set and loops through, removing any value pair that is dominated by another
+    tree: holds "coordinate pairs" and uses nearest neighbor/ discard methods to iteratively solve the skyline
     @return: list of value pairs that corresponds to a perfect skyline of the data set
 
     """
     skyline = []
+    tree = []
+    maxMiles = 0 # Holds max X value in dataset
+    maxPrice = 0 # Holds max Y value in dataset
 
-    # Create tree and fill with every element from dataSet
-    tree = index.Index()
+    # Fill tree with "coordinates" from dataset
     for i in range(len(dataSet)):
-        tree.insert(i, (*dataSet[i], *dataSet[i])) # insert(index, (left, bottom, right, top coordinates))
-
-    # Maximum values of the factors
-    maxMiles = tree.bounds[2] 
-    maxPrice = tree.bounds[3]
+        tree.append(dataSet[i])
+        maxMiles = max(maxMiles, dataSet[i][0])
+        maxPrice = max(maxPrice, dataSet[i][1])
 
     # 'Waiting' queue holds coordinates describing a rectangular area
-    waiting = [(0, 0, maxMiles, maxPrice)] 
+    waiting = [(0, 0, maxMiles, maxPrice)]
 
     # Create Skyline; continue partitioning until no elements remain in the tree
     while len(waiting) > 0:
-        leftOrig, bottomOrig, rightOrig, topOrig = waiting.pop(0) # Coordinates of the current area in which we are searching
-        region = list(tree.intersection((leftOrig, bottomOrig, rightOrig, topOrig))) # List of tree element indices within the current area
+        # Pop coordinates of current region, find the nearest neighbor to bottom left of the current region
+        leftOrig, bottomOrig, rightOrig, topOrig = waiting.pop(0) 
+        newNode = nearestNeighbor(leftOrig, bottomOrig, rightOrig, topOrig, tree)
 
-        # If the current area is not empty, find the skyline element within 
-        if len(region) > 0:
-            
-            # Brute force way to ensure that the nearest neighboring point is within the same region
-            closestPoints = list(tree.nearest((leftOrig, bottomOrig, leftOrig, bottomOrig), tree.count((0, 0, maxMiles, maxPrice))))
-            for i in closestPoints:
-                if i in region:
-                    newIndex = i # Index of nearest neighbor in same region
-                    break
-
-            # Add nearest neighbor to skyline, and remove from tree
-            skyline.append(dataSet[newIndex])
-            tree.delete(newIndex, (*dataSet[newIndex], *dataSet[newIndex]))
-
-            # Remove all tree elements that are guaranteed to be dominated by the new skyline element
-            discardRegion = list(tree.intersection((*dataSet[newIndex], maxMiles, maxPrice)))
-            for i in discardRegion:
-                tree.delete(i, (*dataSet[i], *dataSet[i]))
-
-        # If the region had multiple elements, partition further (add new origin coordinates to the waiting queue)
-        if len(region) > 1:
-            waiting.append((0, dataSet[newIndex][1], dataSet[newIndex][0], maxPrice)) # Create new region above
-            waiting.append((dataSet[newIndex][0], 0, maxMiles, dataSet[newIndex][1])) # Create new region to the left
-            
+        # If the region contains a nearest neighbor, add it to the skyline, branch to new regions, and discard dominated values from tree
+        if newNode != ():
+            skyline.append(newNode)
+            waiting.append((leftOrig, newNode[1], newNode[0], maxPrice)) # Create new region above newest element
+            waiting.append((newNode[0], bottomOrig, maxMiles, newNode[1])) # Create new region to left of newest element
+            discard(newNode, tree) # Remove all elements in tree dominated by the newest element (and remove newest element)
     return skyline
+
+def nearestNeighbor(leftBound, bottomBound, rightBound, topBound, tree):
+    """
+    Find the nearest neighbor to the bottom left of the region passed
+
+    @param leftBound: Leftmost bound of the current region of search
+    @param bottomBound: Lower bound of the current region of search
+    @param rightBound: Rightmost bound of the current region of search
+    @param topBound: Upper bound of the current region of search
+    @param tree: "Tree" of coordinates from dataset that could possibly be in the skyline
+    @return nearest: coordinate pair that is within the region of search, and closest to the origin
+    """
+    # Keep track of nearest element and smallest distance to origin
+    nearest = ()
+    dist = sys.maxsize
+
+    # Loop tgrough remaining elements in tree, ensure it is within the region, and check if it is the nearest neighbor
+    for i in range(len(tree)):
+        # Check if current coordinates are within region
+        if(tree[i][0] >= leftBound and tree[i][0] <= rightBound and tree[i][1] >= bottomBound and tree[i][1] <= topBound):
+
+            # Calculate distance from origin; if shortest so far, update new distance and nearest neighbor
+            newDist = math.sqrt(math.pow(abs(tree[i][0] - leftBound), 2) + math.pow(abs(tree[i][1] - bottomBound), 2))
+            if dist > newDist:
+                dist = newDist
+                nearest = tree[i]
+    return nearest
+
+def discard(origin, tree):
+    """
+````Discard all elements from the "tree" if they are dominated, or the element just added to the skyline
+
+    @param origin: Tuple of the coordinates that has just been added to the skyline
+    @return: none
+    """
+    # Loop through entire tree; if dominated, remove
+    i = 0
+    while i < len(tree):
+        # Remove dominated elements and element that has just been added to skyline
+        if tree[i][0] >= origin[0] and tree[i][1] >= origin[1]:
+            del(tree[i])
+        else:
+            i = i + 1
+    return
+            
+                                
+                           
+        
